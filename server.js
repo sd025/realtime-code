97159a32a521142823eb59b6779e859f8e1ec483
sd025 <shubham.dhumal452@gmail.com>
2024-04-21 19:22:47 +0530

    production ready

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("build"));
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
